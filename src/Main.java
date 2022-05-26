0125b5b31e7f9e19e913c6d14a006955ae02c883
KettiTa <katie.tabune@gmail.com>
2022-05-26 21:39:59 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "! Do you have account with us?");
+        String account = sc.nextLine();
+        if (account.equals("yes")) {
+            System.out.println("Great! Please enter your account number!");
+        } else if (account.equals("no")) {
+            System.out.println("Do you want to create a new account?");
+        } else {
+            System.out.println("Please contact our customer support.");
+        }
+        String info =sc.nextLine();
+        System.out.println("Thank you!");
+
+        System.out.println("Can I help you with anything else?");
+        String q = sc.nextLine();
+        if (q.equals("yes")) {
+            System.out.println("What is your question?");
+        } else  {
+            System.out.println("Have a good day!");
+        }
+
+
     }
 }
